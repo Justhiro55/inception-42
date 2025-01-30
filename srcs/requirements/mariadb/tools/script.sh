316ab7aa65ac4aa0d7a6ac72db55b1ad1fdb8ea2
JustHiro55 <hagi.hiro9829@gmail.com>
2025-01-30 19:34:59 +0900

    feat: Enable configuration via environment variables

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 mysql_install_db
-mysqld
+
+mysqld_safe &
+
+mysql -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE ${MYSQL_DATABASE};
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
+FLUSH PRIVILEGES;
+EOF
+
+wait
