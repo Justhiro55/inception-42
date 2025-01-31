f73546cb867b87e4f9009a476e86df0703280cc8
JustHiro55 <hagi.hiro9829@gmail.com>
2025-01-31 16:36:39 +0900

    fix: resolve errors caused by differences between latest and stable PHP versions

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,4 +6,4 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
-php-fpm8.2 -F
+php-fpm7.4 -F
