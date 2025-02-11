fb9d4db29a26dc56cb3b5df975301bd2f7d24367
JustHiro55 <hagi.hiro9829@gmail.com>
2025-02-11 14:54:00 +0900

    refactor: Simplify password configuration by removing redundancy

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -7,7 +7,7 @@ mysqld_safe &
 # Wait for the MySQL server to be ready
 until mysqladmin ping >/dev/null 2>&1; do
     echo "Waiting for MariaDB to be ready..."
-    sleep 2
+    sleep 1
 done
 
 mysql -u root <<EOF
@@ -15,7 +15,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE ${MYSQL_DATABASE};
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
 FLUSH PRIVILEGES;
 EOF
 
